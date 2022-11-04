8ac4492493f791279626ce9331c290a81ee8b28b
Jiaqi Ye <jye387@gmail.com>
2022-11-04 17:13:01 -0400

    Updated database.js - user and comment CRUD

--- database.js
+++ database.js
@@ -3,17 +3,41 @@
 export function createUserObj(username, password, displayName) {
 
 }
+export function getUserInfo(username) {
+
+}
+export function updateDescriptionObj(text) {
+
+}
+export function updateLocationObj(text) {
+
+}
+export function updateProfilePictureObj(username, path, blob) {
+
+}
 export function existsUser(username) {
 
 }
 export function deleteUserObj(username) {
-    
+
 }
 
 // Comments
 export function createCommentObj(sender, receiver, recipeId, text) {
 
 }
-export function existsRecipe() {
+export function getCommentInfo(commentId) {
+
+}
+export function existsRecipe(username, recipeId) {
+
+}
+export function existsComment(commentId) {
+
+}
+export function updateCommentObj(id, text) {
+
+}
+export function deleteCommentObj(commentId) {
 
 }
