7158c7ed8a5288d196e3137f1b7ea82ce372b78f
Kasagi009 <ddangi@umass.edu>
2022-11-06 17:48:10 -0500

    test

--- database.js
+++ database.js
@@ -2,18 +2,18 @@
 // User
 export function createUserObj(username, password, displayName) {
     console.log("'" + username + "' is added to the database");
-    // if (username == 'Bella') {
-    //     return {Status: 'ERROR', Username: username, errMessage: "Username '" + username + "' already exists"}
-    // }
+    if (username == 'Bella') {
+        return {Status: 'ERROR', Username: username, errMessage: "Username '" + username + "' already exists"}
+    }
     return {Status: 'SUCCESS', Username: username}
 }
 
 // Recipe
 export function createRecipeObj(title, author, ingredients, instructions) {
     console.log("'" + title + ":" + author + ":" + ingredients + ":" + instructions + "'");
-    if (title == 'Pizza' && author == "Bella") {
-        console.log("in here");
-        return {Status: 'ERROR', Recipename: 'MyPiza', errMessage: "Recipename '" + title + "' already exists with id " + 1987}
-    }
+    // if (title == 'Pizza' && author == "Bella") {
+    //     console.log("in here");
+    //     return {Status: 'ERROR', Recipename: 'MyPiza', errMessage: "Recipename '" + title + "' already exists with id " + 1987}
+    // }
     return {Status: 'SUCCESS', Recipename: 'MyPiza', recipeId: '1987'}
 }
