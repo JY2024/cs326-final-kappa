f04a257904bbdc14a278da4b91ae620e72a8e3da
Kasagi009 <ddangi@umass.edu>
2022-11-05 21:22:11 -0400

    update on homepage

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import * as server from './server.js'
 const app = express();
 app.use(express.json());
 const port = 3000;
-const path = require("path");
+const path = require('path');
 
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, './html_files', 'profile.html'));
