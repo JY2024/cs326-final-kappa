8077600fa4aef37f1639b40bc937958289dcdae3
Kasagi009 <ddangi@umass.edu>
2022-11-06 22:37:03 -0500

    test

--- server.js
+++ server.js
@@ -36,7 +36,8 @@ import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-express.static(path.join(__dirname, 'RecipeTest'));
+app.use('/static', express.static(path.join(__dirname, 'RecipeTest')));
+// express.static(path.join(__dirname, 'RecipeTest'));
 
 //ROUTES
 app.get('/', (req, res) => {
