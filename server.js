748a6c301bc65c69e0b7c445756663819f3ad955
Kasagi009 <ddangi@umass.edu>
2022-11-06 23:18:53 -0500

    test

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/', (req, res) => {
 
 app.get('/', (req, res) => {
     console.log('At home');
-    res.sendFile(path.join(__dirname, './htmlFiles/index.html'));
+    res.sendFile(path.join(__dirname, '/htmlFiles/index.html'));
 });
 
 app.get('/profile.html', (req, res) => {
