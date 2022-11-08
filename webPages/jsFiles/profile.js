7c789490725632808806d6ee239383057443c9bf
Kasagi009 <ddangi@umass.edu>
2022-11-07 21:00:35 -0500

    fixing links

--- webPages/jsFiles/profile.js
+++ webPages/jsFiles/profile.js
@@ -2,5 +2,5 @@ const temp = document.getElementById('settings');
     temp.addEventListener('click', () =>{
         console.log("settings change");
 
-        window.location = "/htmlFiles/profile-settings-personal-info.html";
+        window.location = "/profile-settings-personal-info.html";
     });
