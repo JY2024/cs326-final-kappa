ef811820a4e33c085285dc06f3d40991ba212301
Kasagi009 <ddangi@umass.edu>
2022-11-17 13:31:02 -0500

    database

--- database.js
+++ database.js
@@ -1,3 +1,23 @@
+const { Client } = require('pg');
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+  
+  client.connect();
+
+//EXAMPLE QUERY SHOWN BELOW:
+
+  //client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
+
 // DataBase Functions, NOTE: Still returns dummy data
 // Authorization
 export function authUserObj(req) {
